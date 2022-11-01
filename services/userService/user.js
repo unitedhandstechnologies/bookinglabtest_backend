cd2bd2ba3d61c2c866dcfed3aeb3729dae580cdc
Sivaprakash <sivaprakash.p@unitedhands.cc>
2022-11-01 19:12:11 +0530

    Added user service

---
+++ services/userService/user.js
@@ -0,0 +1,27 @@
+const db = require("../../config/dbConfig");
+const exception = require("../../constants/exception.json");
+
+const createUser = async (req, res) => {
+    try {
+      const user = req.body;
+      const existMobileNumber = await db.query(
+        `SELECT * FROM users WHERE mobile_number = $1;`,
+        [user.mobile_number]
+      );
+  
+      if (existMobileNumber.rowCount != 0)
+        return res
+          .status(400)
+          .send({message:exception.alreadyExist});
+          
+      const newUser = await db.query(
+        `INSERT INTO users (mobile_number) VALUES('${user.mobile_number}') RETURNING *`
+      );
+  
+      return res.status(201).send({user:newUser.rows[0]});
+  
+    } catch (err) {
+      console.log(err);
+      return res.status(500).send({Error:err});
+    }
+  };
